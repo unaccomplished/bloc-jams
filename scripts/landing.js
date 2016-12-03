3875cf1200d5035e992716f05fb33d63b8b90a97
Jocelyn Hsu <hsu.joce@gmail.com>
2016-12-03 15:52:12 -0800

    Checkpoint 23 DOM Scripting: Events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -12,3 +12,19 @@ var animatePoints = function() {
         revealPoint(i);
     }
 };
+
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
